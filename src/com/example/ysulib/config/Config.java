7c3ce0bb6c02cd3773ab296671eaf0177185b916
lixing <myxingxing66@126.com>
2016-08-11 16:14:25 +0800

    需添加Bmob的APPID

--- src/com/example/ysulib/config/Config.java
+++ src/com/example/ysulib/config/Config.java
@@ -3,7 +3,7 @@ package com.example.ysulib.config;
 //系统变量
 public class Config {
 
-	public static String applicationId = "";
+	public static String applicationId = ""; 
 	
 	public static String Bmob_APPID = "";// bmob sdk的APPID
 	
